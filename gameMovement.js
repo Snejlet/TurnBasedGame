e039e310a528e8ca26d4731411cfe1edc4b04ed7
Snejlet <seanp_james@hotmail.com>
2017-11-11 04:49:34 +0000

    A few extra console.log lines and proper commenting

--- gameMovement.js
+++ gameMovement.js
@@ -1,7 +1,11 @@
+// The checkCollision functions check which tile the character is on AFTER they move, and if it is a collision tile,
+// moves them back to the tile they just left (so it appears as if they haven't moved.)
 function checkCollisionUp() {
 
     if (mapGrid[playerOneX][playerOneY] === 5) {
+        //console.log("Ended movement on "+playerOneX+","+playerOneY);
         playerOneX++;
+        //console.log("Moved back to "+playerOneX+","+playerOneY)
     }
 }
 
@@ -23,12 +27,14 @@ function checkCollisionRight() {
     }
 }
 
+// looks for a keyup event
 window.addEventListener("keyup", function(event) {
-
+// checks which player's turn it is, and moves the appropriate sprite
     if (playerTurn === 1) {
-        console.log(event.keyCode + "(Player 1)");
+        //console.log(event.keyCode + "(Player 1)");
         switch (event.keyCode) {
             case 87:
+                // checks if the player is about to move off the map. If not: allows move. if so: prevents move.
                 if (playerOneX > 0) {
                     playerOneX--;
                     checkCollisionUp();
